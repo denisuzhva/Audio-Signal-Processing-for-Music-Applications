8d646405ac5fa97da726f9952b33c899510ccf5a
mortarsynth <denis.uzhva@yahoo.com>
2020-01-24 19:26:57 +0300

    A7

---
+++ A6/dos2unix.py
@@ -0,0 +1,17 @@
+"""
+convert dos linefeeds (crlf) to unix (lf)
+usage: dos2unix.py 
+"""
+original = "testInputA6.pkl"
+destination = "testInputA6.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+print("Done. Saved %s bytes." % (len(content)-outsize))
---
+++ README.md
@@ -0,0 +1,2 @@
+# Audio-Signal-Processing-for-Music-Applications
+Messing up with the Coursera course "Audio Signal Processing for Music Applications"
